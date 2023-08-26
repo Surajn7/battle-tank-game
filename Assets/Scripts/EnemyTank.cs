185b37d98096dc0027e1d30ee6f55c786aa0e6bf
Surajn7 <105142430+Surajn7@users.noreply.github.com>
2023-08-26 15:15:58 +0530

    Generic Class
    Generic Class

---
+++ Assets/Scripts/EnemyTank.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTank : SingletonGeneric<EnemyTank>
+{
+    void Enemy()
+    {
+        Debug.Log("Enemy ");
+    }
+}
